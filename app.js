a7362b8c30c2a5353bcd5d41b12ebeec6d92b095
jfrey256 <jessefreyyy@gmail.com>
2023-02-11 14:50:14 -0600

    Added reset button, rainbow color choice, hold down mouse to draw instead of hovering, text that displays active color and if you are actively drawing.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const newSize = document.querySelectorAll(".button");
+let color = "black";
+
+let isMouseDown = false;
 
 function makeBoard(size) {
     let board = document.querySelector(".board");
@@ -9,13 +12,24 @@ function makeBoard(size) {
 
     let amount = size * size;
 
+
+    board.addEventListener('mousedown', function() {
+        isMouseDown = true;
+        drawingMode();
+    });
+
+    board.addEventListener('mouseup', function() {
+        isMouseDown = false;
+        drawingMode();
+    });
+
     for (let i = 0; i < amount; i ++) {
         let div = document.createElement("div");
         div.addEventListener('mouseover', colorSquare)
         div.style.borderStyle = "solid";
         div.style.borderColor = "grey";
         board.insertAdjacentElement('beforeend',div);
-    }
+   }
 }
 
 makeBoard(16);
@@ -25,8 +39,53 @@ makeBoard(16);
     makeBoard(choice);
 }))
 
+// Random RGB coloring
+const randomColorButton = document.querySelector(".randomColor");
+randomColorButton.addEventListener("click", () => {
+    color = "random";
+});
+
 function colorSquare() {
-    this.style.backgroundColor = "black";
+    if (isMouseDown) {
+        if (color === "random") {
+            this.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
 }
+
+// Display current color selection
+let colorMode = document.querySelector(".colorMode");
+let colorClicked = document.querySelector(".color");
+function changeColor(colorChoice) {
+    color = colorChoice
+    colorMode.textContent = `Color: ${color}`;
+}
+
+
+
+// Display drawing mode and if active
+
+let drawing = document.querySelector(".drawing");
+
+function drawingMode() {
+    if (isMouseDown) {
+        drawing.textContent = "Drawing...";
+    } else {
+        drawing.textContent = null;
+    }
+}
+
+// Reset board
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+document.body.insertAdjacentElement('beforeend', resetButton);
+resetButton.addEventListener("click", () => {
+  makeBoard(16);
+});
+
+
 
 
